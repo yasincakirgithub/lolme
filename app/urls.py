afbbe2725cc03a58511af6e9eca1478f4cc23d2d
Yasin Çakır <yasincakir.py@gmail.com>
2023-03-21 16:18:25 +0300

    about page

--- app/urls.py
+++ app/urls.py
@@ -4,6 +4,7 @@ from app.views import *
 urlpatterns = [
     path('', homePage,name='homePage'),
     path('about/', aboutPage,name='aboutPage'),
+    path('summoner/<region>/<summoner_name>/', summonerPage,name='summonerPage'),
     # path('iha/add/', ihaAddPage,name='ihaAddPage'),
     # path('iha/list/', ihaListPage,name='ihaListPage'),
     # path('iha/update/<id>/', ihaUpdatePage,name='ihaUpdatePage'),
--- app/views.py
+++ app/views.py
@@ -7,3 +7,12 @@ def homePage(request):
 
 def aboutPage(request):
     return render(request,'about/index.html',context={})
+
+
+def summonerPage(request,region,summoner_name):
+    print(region,summoner_name)
+    context = {
+        'region':region,
+        'summoner_name':summoner_name
+    }
+    return render(request,'summoner/index.html',context)
